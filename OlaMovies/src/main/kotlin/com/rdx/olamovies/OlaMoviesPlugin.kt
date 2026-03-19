672f4c7306f1fd3026413f072be2ff15c5cb05dd
rdx517895-gif <rdx517895@gmail.com>
2026-03-20 02:44:17 +0530

    Update final OlaMoviesPlugin.kt

--- OlaMovies/src/main/kotlin/com/rdx/olamovies/OlaMoviesPlugin.kt
+++ OlaMovies/src/main/kotlin/com/rdx/olamovies/OlaMoviesPlugin.kt
@@ -2,11 +2,10 @@ package com.rdx.olamovies
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
-import android.content.Context
 
 @CloudstreamPlugin
 class OlaMoviesPlugin : BasePlugin() {
-    override fun load(context: Context) {
+    override fun load() {
         registerMainAPI(OlaMoviesProvider())
     }
 }
