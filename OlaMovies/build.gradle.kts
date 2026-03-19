42c6a1761700e2d7eadd6fe316baaa87a12865f7
rdx517895-gif <rdx517895@gmail.com>
2026-03-20 02:13:24 +0530

    Create build.gradle.kts

---
+++ OlaMovies/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "OlaMovies – 4K UHD Google Drive Movies & Series"
+    authors      = listOf("rdx517895-gif")
+    language     = "hi"
+    status       = 1
+    tvTypes      = listOf("Movie", "TvSeries")
+    iconUrl      = "https://n1.olamovies.info/favicon.ico"
+}
