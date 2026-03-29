5c655de3d5665ac00162b5da65c28e77a585ef19
rdx517895-gif <rdx517895@gmail.com>
2026-03-29 10:58:48 +0530

    Create build.gradle.kts

---
+++ OlaMovies/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 3
+cloudstream {
+    language = "en"
+    description = "CineVood - Bollywood Movies & Series"
+    authors = listOf("rdx517895")
+
+    status = 1 // 0: Down, 1: Ok, 2: Slow, 3: Beta
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=1cinevood.watch&sz=%size%"
+}
